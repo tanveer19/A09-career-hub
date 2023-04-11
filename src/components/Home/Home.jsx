bc3ea4a5bd83d74021abd950911e2123a1dd75c4
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-04-11 08:21:46 +0600

    header done

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "./Home.css";
-import { Container, Row } from "react-bootstrap";
 
 const Home = () => {
   return (
-    <div>
-      <div className="">
-        <h2>home</h2>
+    <div className="container">
+      <div className="row">
+        <div className="col-6 text-center">
+          <h1 className="">
+            One Step <br /> Closer To Your <br /> <span>Dream Job</span>
+          </h1>
+          <p>
+            Explore thousands of job opportunities with all the information you
+            need. Its your future. Come find it. Manage all your job application
+            from start to finish.
+          </p>
+          <button type="button" className="btn btn-primary">
+            Get Started
+          </button>
+        </div>
+        <div className="col-6">
+          <img src="https://picsum.photos/id/20/500/300" alt="" />
+        </div>
       </div>
     </div>
   );
