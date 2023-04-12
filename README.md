e4dff0a6321e813b089caa38f864f6715ff7c73f
Tanveer Hossain Jony <thjbd19@gmail.com>
2023-04-12 23:23:50 +0600

    updated Readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 "# b7a9-career-hub-tanveer19"
 
 Repo link: https://github.com/Porgramming-Hero-web-course/b7a9-career-hub-tanveer19
+
+Live site: https://lucent-moonbeam-05af63.netlify.app/
--- src/components/Job/Job.jsx
+++ src/components/Job/Job.jsx
@@ -8,15 +8,15 @@ const Job = ({ job }) => {
         <img className="img-fluid" src={job.companyLogo} alt="logo" />
         <h6>{job.jobTitle}</h6>
         <p>{job.companyName}</p>
-        <button type="button" className="btn btn-primary">
+        <button type="button" className="btn btn-light">
           {job.remote}
         </button>
-        <button type="button" className="btn btn-primary">
+        <button type="button" className="btn btn-light">
           {job.time}
         </button>
         <br />
         <span>{job.location}</span> <span>{job.salary}</span> <br />
-        <button type="button" className="btn btn-primary">
+        <button type="button" className="btn btn-light">
           <Link to={`/job/${job.id}`}>View Details</Link>
         </button>
       </div>
