1ef92dd1f1f2b888ff35ed9f7dd626cc6bbb8ba9
Tanveer Hossain Jony <thjbd19@gmail.com>
2024-09-26 22:12:56 +0600

    temp

--- src/components/AppliedJobs/AppliedJobs.jsx
+++ src/components/AppliedJobs/AppliedJobs.jsx
@@ -2,16 +2,28 @@ import React from "react";
 import { useEffect } from "react";
 import { useLoaderData } from "react-router-dom";
 import { getStoredJobApplications } from "../../utility/localstorage";
+import { useState } from "react";
 
 const AppliedJobs = () => {
   const jobs = useLoaderData();
 
+  const [appliedJobs, setAppliedJobs] = useState([]);
+
   useEffect(() => {
     const storedJobIds = getStoredJobApplications();
     if (jobs.length > 0) {
-      const jobsApplied = jobs.filter((job) => console.log(job));
+      // const jobsApplied = jobs.filter((job) => storedJobIds.includes(job.id));
+      const jobsApplied = [];
+      for (const id of storedJobIds) {
+        const job = jobs.find((job) => job.id === id);
+        if (job) {
+          jobsApplied.push(job);
+        }
+      }
+      console.log(jobs, storedJobIds, jobsApplied);
     }
   }, []);
+
   return (
     <div>
       <h2>Applied Jobs</h2>
